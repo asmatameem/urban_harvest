529f3c8289c7d8db8a1e6be3e4167e7239926d35
Asma Tameem <atameem@spokeo.com>
2014-03-15 17:37:16 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrbanHarvest::Application.config.secret_token = '50a1f6a173c12f07c886ad6df4147f3afcc4b27499535a00dc823dd9d27b45aacbe418201d4d904615dc6223ce1b2d5466ebf4bd0348012c9919584b4d1d4903'
